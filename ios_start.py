85a35d394c9e3fedf7a50ae3b959745a38d0873c
lixingda <lixingda@wb-lixingda.local>
2019-12-06 16:56:00 +0800

    Initial commit

---
+++ ios_start.py
@@ -0,0 +1,16 @@
+from appium import webdriver
+
+
+def start_tapp():
+    caps = {
+        'platformName': 'ios',
+        'platformVersion': '10',
+        'deviceName': '“纪淼”的 iPhone',
+        'app': 'com.jiangjia.gif',
+        'udid': '9380f2f029afff8b022165223d2b40dc7966acc6',
+    }
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', caps)
+
+
+if __name__ == '__main__':
+    start_tapp()
