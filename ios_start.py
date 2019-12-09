0bb579fc2a1eb956b95d54b0825716a2a5c8c0b5
lixingda <775325523@qq.com>
2019-12-09 11:30:53 +0800

    dele sure.py

--- ios_start.py
+++ ios_start.py
@@ -5,9 +5,9 @@ def start_tapp():
     caps = {
         'platformName': 'ios',
         'platformVersion': '10',
-        'deviceName': 'xxxxx',
-        'app': 'xxxxxx',
-        'udid': 'xxxxxxxx',
+        'deviceName': '“纪淼”的 iPhone',
+        'app': 'com.jiangjia.gif',
+        'udid': '9380f2f029afff8b022165223d2b40dc7966acc6',
     }
     driver = webdriver.Remote('http://localhost:4723/wd/hub', caps)
 
